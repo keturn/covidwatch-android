00ab2000109f7f06780093ff0debd3b7e822ac33
Akhil Veeraghanta <akhilveeraghanta@Akhils-iPhone.hitronhub.home>
2020-03-09 12:29:32 -0700

    wip

--- ContactTracing/app/src/main/java/com/riskre/contact_tracing/MainActivity.java
+++ ContactTracing/app/src/main/java/com/riskre/contact_tracing/MainActivity.java
@@ -2,13 +2,172 @@ package com.riskre.contact_tracing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseSettings;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.BeaconTransmitter;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+import org.altbeacon.beacon.startup.RegionBootstrap;
+
+import java.util.Arrays;
+
+import static org.altbeacon.beacon.BeaconTransmitter.NOT_SUPPORTED_BLE;
+import static org.altbeacon.beacon.BeaconTransmitter.NOT_SUPPORTED_CANNOT_GET_ADVERTISER;
+import static org.altbeacon.beacon.BeaconTransmitter.NOT_SUPPORTED_CANNOT_GET_ADVERTISER_MULTIPLE_ADVERTISEMENTS;
+import static org.altbeacon.beacon.BeaconTransmitter.NOT_SUPPORTED_MIN_SDK;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private RegionBootstrap regionBootstrap;
+
+    // just owning an instance of this will save battery
+    private BackgroundPowerSaver backgroundPowerSaver;
+    private boolean haveDetectedBeaconsSinceBoot = false;
+    private String cumulativeLog = "";
+    Beacon beacon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BeaconManager beaconManager = org.altbeacon.beacon.BeaconManager.getInstanceForApplication(this);
+
+        // set iBeacon layout to be compatible across platforms
+        beaconManager.getBeaconParsers().clear();
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+
+        beaconManager.setDebug(true);
+
+        Notification.Builder builder = new Notification.Builder(this);
+        builder.setContentTitle("Passively logging human contacts...");
+        Intent intent = new Intent(this, MonitoringActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        builder.setContentIntent(pendingIntent);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("My Notification Channel ID",
+                    "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("My Notification Channel Description");
+            NotificationManager notificationManager = (NotificationManager) getSystemService(
+                    Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel);
+            builder.setChannelId(channel.getId());
+        }
+
+        beaconManager.enableForegroundServiceScanning(builder.build(), 456);
+
+        // For the above foreground scanning service to be useful, disable
+        // JobScheduler-based scans and set a fast background scan
+        // cycle that would otherwise be disallowed by the operating system.
+        beaconManager.setEnableScheduledScanJobs(false);
+        beaconManager.setBackgroundBetweenScanPeriod(0);
+        beaconManager.setBackgroundScanPeriod(1100);
+
+        Log.d(TAG, "setting up background monitoring for beacons and power saving");
+
+        // wake up the app when a beacon is seen
+        Region region = new Region("backgroundRegion", null, null, null);
+        regionBootstrap = new RegionBootstrap(this, region);
+s
+        // simply constructing this class and holding a reference to it in your custom Application
+        // class will automatically cause the BeaconLibrary to save battery whenever the application
+        // is not visible.  This reduces bluetooth power usage by about 60%
+        backgroundPowerSaver = new BackgroundPowerSaver(this);
+
+        initializeTransmitBeacon();
+    }
+
+    /*
+        Starts the low power beacon transmission
+
+        @param TODO
+        @return TODO
+     */
+    public void initializeTransmitBeacon() {
+
+        int result = BeaconTransmitter.checkTransmissionSupported(this);
+
+        // TODO error handling, for now we assume device has the required hardware
+        switch(result) {
+            case NOT_SUPPORTED_MIN_SDK:
+                // TODO error handling
+                break;
+            case NOT_SUPPORTED_BLE:
+                // TODO error handling
+                break;
+            case NOT_SUPPORTED_CANNOT_GET_ADVERTISER_MULTIPLE_ADVERTISEMENTS:
+                // TODO error handling
+                break;
+            case NOT_SUPPORTED_CANNOT_GET_ADVERTISER:
+                // TODO error handling
+                break;
+        }
+
+        String deviceUniqueIdentifier = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+
+        beacon = new Beacon.Builder()
+                .setId1("ctr_"+deviceUniqueIdentifier)
+                .setId2("1") // TODO useful?
+                .setId3("2") // TODO useful?
+                .setManufacturer(0x0118) // Radius Networks
+                .setTxPower(-59) // low power
+                .setDataFields(Arrays.asList(new Long[] {0l}))
+                .build();
+
+        // create an iBeacon
+        BeaconParser beaconParser = new BeaconParser()
+                .setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24");
+
+        BeaconTransmitter beaconTransmitter = new BeaconTransmitter(getApplicationContext(), beaconParser);
+
+        beaconTransmitter.startAdvertising(beacon, new AdvertiseCallback() {
+            @Override
+            public void onStartFailure(int errorCode) {
+                Log.e(TAG, "Advertisement start failed with code: "+errorCode);
+            }
+            @Override
+            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                Log.i(TAG, "Advertisement start succeeded.");
+            }
+        });
+    }
+
+    public void disableMonitoring() {
+        if (regionBootstrap != null) {
+            regionBootstrap.disable();
+            regionBootstrap = null;
+        }
+    }
+    public void enableMonitoring() {
+        Region region = new Region("backgroundRegion",
+                null, null, null);
+        regionBootstrap = new RegionBootstrap(this, region);
+    }
+
+    public void checkContactLogs(View view){
+        Intent intent = new Intent(this, ContactLogs.class);
+        startActivity(intent);
     }
 }
